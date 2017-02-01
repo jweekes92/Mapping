b0a8358673ecb69af25c938a78a63c70bb165f85
jordan weekes <1weeka52@solent.ac.uk>
2017-02-01 14:56:52 +0000

    Lat and Long

--- app/src/main/java/com/example/a1weeka52/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1weeka52/mapping/HelloMap.java
@@ -3,12 +3,17 @@ package com.example.a1weeka52.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 
-public class HelloMap extends Activity
+public class HelloMap extends Activity implements OnClickListener
 {
 
     MapView mv;
@@ -26,5 +31,20 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        TextView tv1 = (TextView)findViewById(R.id.latv);
+        TextView tv2 = (TextView)findViewById(R.id.lonv);
+        EditText lat = (EditText)findViewById(R.id.et1);
+        EditText lon = (EditText)findViewById(R.id.et2);
+
+        double latitude = Double.parseDouble(lat.getText().toString());
+        double longitude = Double.parseDouble(lon.getText().toString());
+
     }
 }
