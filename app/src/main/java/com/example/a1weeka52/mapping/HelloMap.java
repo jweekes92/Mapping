e6c33843a62913d5297543fd017b2880dddf49cf
jweekes92 <1weeka52@solent.ac.uk>
2017-02-06 13:12:28 +0000

    Map choose activity

--- app/src/main/java/com/example/a1weeka52/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1weeka52/mapping/HelloMap.java
@@ -1,8 +1,12 @@
 package com.example.a1weeka52.mapping;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,6 +38,27 @@ public class HelloMap extends Activity implements OnClickListener
 
         Button b = (Button)findViewById(R.id.btn1);
         b.setOnClickListener(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            //System.exit(0);
+
+            Intent intent = new Intent(this, MapChooseActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
     }
 
     public void onClick(View view)
---
+++ app/src/main/java/com/example/a1weeka52/mapping/MapChooseActivity.java
@@ -0,0 +1,20 @@
+package com.example.a1weeka52.mapping;
+
+/**
+ * Created by 1weeka52 on 06/02/2017.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class MapChooseActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mca);
+    }
+}
